d6e5188bfe6cceb5941f1d97f46b89fc2e2e5c57
Varun <varun.nagrare@gmail.com>
2020-07-01 02:25:50 +0530

    ADDED: Bucket Creation on AWS

---
+++ CreationPhase/createBucket.py
@@ -0,0 +1,18 @@
+import boto3
+
+bucket = input("\nEnter Bucket name: ")
+s3 = boto3.resource('s3')
+
+
+def createBucket():
+    try:
+        s3.create_bucket(Bucket=bucket,
+                         ACL='public-read-write',
+                         CreateBucketConfiguration={'LocationConstraint': 'ap-south-1'}
+                         )
+        return print("Bucket created Successfully!")
+
+    except Exception:
+        return print("Bucket already exists!")
+
+# 'trialbucket-example'
